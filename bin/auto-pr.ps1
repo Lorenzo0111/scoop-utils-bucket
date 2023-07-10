8c34c664404a0fd9f994bbdd2f12ad179f7947fc
Lorenzo <69311874+Lorenzo0111@users.noreply.github.com>
2023-07-10 15:17:03 +0200

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "Lorenzo0111/scoop-utils-bucket:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
